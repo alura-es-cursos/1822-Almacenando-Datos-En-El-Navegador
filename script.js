684d216a3107db122f62e7dd7a949393150ac375
Juan Santa <jcsantahurtado@gmail.com>
2022-03-21 15:47:57 -0500

    01. Trabajando con fechas

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento) => {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+
+}
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
+
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
@@ -21,10 +31,15 @@ const createTask = (evento) => {
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
 
+  const dateElement = document.createElement('span');
+  dateElement.innerHTML = dateFormat;
+
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);

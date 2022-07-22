1e18d3ac987c2338b77c24e74c671763b5a75fcf
Pablo Zabala <could_2032@outlook.com>
2022-07-22 11:07:53 -0300

    agregando fecha  y hora al creador de tareas

--- script.js
+++ script.js
@@ -2,29 +2,40 @@ import checkComplete from './components/checkComplete.js';
 import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
+    
+const addTask = (evento) =>{
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+
+}
+
 
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector("[data-form-date]");
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value;
+const dateFormat = moment(date).format("DD/MM/YYYY");
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
   //backticks
   const taskContent = document.createElement('div');
-
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
-  task.appendChild(taskContent);
+   const dateElement = document.createElement("span");
+   dateElement.innerHTML = dateFormat;
+   task.appendChild(taskContent);
+   task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
